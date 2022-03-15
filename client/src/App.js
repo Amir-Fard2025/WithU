9947c558191f5e6084c51085c6984b3c92954fed
jitasek <jitka.moreira@gmail.com>
2022-03-15 22:27:12 +0100

    finalize footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import ResponsiveNavbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <ResponsiveNavbar />
+      <Footer />
     </div>
   );
 }
