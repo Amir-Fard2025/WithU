e9c0221f3ff4bb01b4dff9015e1c572eec2d4d79
Olivier R.Measson <olivierberlin1998@hotmail.de>
2022-03-16 16:52:00 +0100

    added searchbar + autocomplete tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import "./App.js";
+import SearchBar from "./components/SearchBar/searchBar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/SearchBar/searchBar.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import "./SearchBar.css";
+
+export default function Tags() {
+  return (
+    <>
+      <Stack
+        className="stack"
+        spacing={3}
+        sx={{ width: 500, color: "primary.main" }}
+      >
+        <Autocomplete
+          className="autocomplete"
+          multiple
+          id="tags-standard"
+          options={topResearch}
+          getOptionLabel={(option) => option.search}
+          defaultValue={[topResearch[13]]}
+          renderInput={(params) => (
+            <TextField
+              className="textfield"
+              {...params}
+              variant="standard"
+              placeholder="How can I help..."
+            />
+          )}
+        />
+      </Stack>
+    </>
+  );
+}
+
+// Top 13 Research dummies
+const topResearch = [
+  { search: "Ukraine" },
+  { search: "Germany" },
+  { search: "Finance" },
+  { search: "Shelters" },
+  { search: "Children" },
+  { search: "Animals" },
+  { search: "Refugees" },
+  {
+    search: "Medical",
+  },
+  { search: "Transport" },
+  { search: "Border" },
+  {
+    search: "Stay",
+  },
+  {
+    search: "Legal",
+  },
+  { search: "Place" },
+  { search: "Help" },
+];
