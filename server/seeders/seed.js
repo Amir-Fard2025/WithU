43d7b8a61115afeee3ecb10dd20bca3e871755a7
pillow <fard.cca@gmail.com>
2022-03-17 19:33:35 +0100

    Add auto-generated tag id while seeding to resourcesCards

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -3,19 +3,42 @@ const db = require("../config/connection");
 const { User, ResourceCard, Tag } = require("../models");
 const userSeeds = require("./userSeeds.json");
 const resourceSeeds = require("./resourceSeeds.json");
-const tags = require("./tagSeeds");
+const tags = require("./tagSeeds.json");
 
 // needs to connect to the database, right?
+const generateRandom = (min, max) => {
+  // find diff
+  let difference = max - min;
+
+  // generate random number
+  let rand = Math.random();
+
+  // multiply with difference
+  rand = Math.floor(rand * difference);
+
+  // add with min value
+  rand = rand + min;
+
+  return rand;
+};
+
+const prepareResourceSeeds = (resourceSeeds, newTags) => {
+  return resourceSeeds.map((rs) => {
+    return { ...rs, tag_id: [newTags[generateRandom(0, tags.length - 1)]._id] };
+  });
+};
 
 db.once("open", async () => {
   try {
     await User.deleteMany({});
     await User.create(userSeeds, { validateBeforeSave: true });
-    await ResourceCard.deleteMany({});
-    await ResourceCard.create(resourceSeeds, { validateBeforeSave: true });
     await Tag.deleteMany({});
-    await Tag.create(tags, { validateBeforeSave: true });
-
+    const newTags = await Tag.create(tags, { validateBeforeSave: true });
+    await ResourceCard.deleteMany({});
+    const updatedResourceSeeds = prepareResourceSeeds(resourceSeeds, newTags);
+    const newCards = await ResourceCard.create(updatedResourceSeeds, {
+      validateBeforeSave: true,
+    });
     console.log("all done!");
     process.exit(0);
   } catch (err) {
