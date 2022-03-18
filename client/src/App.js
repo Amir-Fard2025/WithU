9f92b2324c9955723259790b2e53f742ef474e2f
Olivier R.Measson <olivierberlin1998@hotmail.de>
2022-03-18 16:55:10 +0100

    added modal with 5 inputs possibilites

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import Modal from "./components/Modal/Modal";
+import React from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div
+      className="App"
+      style={{
+        background: "linear-gradient(#4285f49f, #ffff00b3)",
+        height: "100vh",
+      }}
+    >
+      <Modal />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,111 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Modal from "@mui/material/Modal";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  opacity: "0.7",
+  border: "0px solid",
+  borderRadius: "20px",
+  color: "white",
+  p: 4,
+};
+
+const fontColor = {
+  color: "white",
+  width: 400,
+};
+
+export default function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        style={{ color: "red" }}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TextField
+            sx={fontColor}
+            id="outlined-basic"
+            label="Title"
+            variant="outlined"
+          />
+          <TextField
+            sx={fontColor}
+            id="outlined-basic"
+            label="Description"
+            variant="outlined"
+          />
+          <TextField
+            sx={fontColor}
+            id="outlined-basic"
+            label="URL"
+            variant="outlined"
+          />
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={topLabel}
+            sx={{ width: 400 }}
+            renderInput={(params) => <TextField {...params} label="Tags" />}
+          />
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={languages}
+            sx={{ width: 400 }}
+            renderInput={(params) => <TextField {...params} label="Language" />}
+          />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+const topLabel = [
+  { label: "Ukraine" },
+  { label: "Children" },
+  { label: "Finance" },
+  { label: "Shelters" },
+  { label: "Germany" },
+  { label: "Animals" },
+  { label: "Refugees" },
+  {
+    label: "Medical",
+  },
+  { label: "Transport" },
+  { label: "Border" },
+  {
+    label: "Stay",
+  },
+  {
+    label: "Legal",
+  },
+  { label: "Place" },
+  { label: "Help" },
+];
+
+const languages = [
+  { label: "English" },
+  { label: "German" },
+
+  { label: "Ukrainian" },
+  { label: "Polish" },
+  { label: "French" },
+];
