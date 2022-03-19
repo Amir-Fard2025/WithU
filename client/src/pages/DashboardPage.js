2d7c1a5a399118633f4f3f67ff6989a0247fbf41
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2022-03-19 12:57:15 +0100

    Creat faq-component, routes in app.js, single pages in pages folder

---
+++ client/src/pages/DashboardPage.js
@@ -0,0 +1,15 @@
+export default function DashboardPage() {
+  return (
+    <>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+      <h1>Hello :)</h1>
+    </>
+  );
+}
---
+++ client/src/pages/FAQPage.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function FAQPage() {
+  return (
+    <Typography component="div">
+      <Box
+        sx={{
+          textAlign: "center",
+          width: "50%",
+          backgroundColor: "transparent",
+          marginTop: "5%",
+          marginLeft: "25%",
+          color: "white",
+        }}
+      >
+        This page is about I am Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Commodi, architecto iusto magnam quidem totam eaque
+        dolorum vel adipisci! Cumque consequuntur debitis tempore blanditiis
+        aliquid perferendis magni nam, nihil explicabo ea!.
+      </Box>
+    </Typography>
+  );
+}
