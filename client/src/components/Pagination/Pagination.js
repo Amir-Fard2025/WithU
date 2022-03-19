77ec94ff2e309eb7f37a4b9fa1a0baa512d350c8
vvicunag <vvicuna1@uc.cl>
2022-03-19 12:11:09 +0100

    add nice pagination and page function

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -3,11 +3,10 @@ import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
 
-export default function PaginationRounded() {
+export default function PaginationRounded({count, updatePage}) {
   return (
     <Stack spacing={2}>
-      <Pagination count={10} shape="rounded" />
-      <Pagination count={10} variant="outlined" shape="rounded" />
+      <Pagination count={count} shape="rounded" onClick={(e)=>updatePage(parseInt(e.target.innerText))} />
     </Stack>
   );
 }
