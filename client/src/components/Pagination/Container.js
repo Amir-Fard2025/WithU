900c39b51307201edd4bbe5bb2cc7d029f0cd60f
vvicunag <vvicuna1@uc.cl>
2022-03-19 10:45:48 +0100

    pagination limit of cards per page

--- client/src/components/Pagination/Container.js
+++ client/src/components/Pagination/Container.js
@@ -5,12 +5,18 @@ import Card from "../Card/Card"
 import {GET_ALL_CARDS} from "../../utils/queries"
 import { useQuery } from "@apollo/client";
 
-
+const CARDS_PER_PAGE = 1;
 const Container = () => {
     const {loading, data, error}= useQuery(GET_ALL_CARDS);
-
+    const [currPage, setCurrPage] = useState(1);
+    const [totalCards, setTotalCards] = useState([]);
     console.log(loading, data);
-
+    let currCards = [];
+    if(totalCards.length === 0  && !loading){ 
+        setTotalCards(data.resourcesCards);
+    }
+    const startingCardIndex = (currPage - 1) * CARDS_PER_PAGE;
+    currCards = data.resourcesCards.slice(startingCardIndex, startingCardIndex + CARDS_PER_PAGE);
 
     if(error) {
         console.log(error);
@@ -21,10 +27,9 @@ const Container = () => {
            </div>
        )
    } else {
-
        return (
            <div>
-            {data.resourcesCards.map(c => <Card key={c.id} title={c.title} description = {c.description} />)};
+            {currCards.map(c => <Card key={c.id} title={c.title} description = {c.description} />)};
         </div>
         
         )
