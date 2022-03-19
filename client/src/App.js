8674391db8a88702d7335f90e9d101540745af62
vvicunag <vvicuna1@uc.cl>
2022-03-19 11:38:22 +0100

    add button functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
 import ResponsiveNavbar from "./components/Navbar/ResponsiveNavbar";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Container from "../src/components/Pagination/Container";
+import Card from "./components/Card/Card";
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <ResponsiveNavbar />
+      <Container/>
       <Footer />
     </div>
+
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Pagination/Container.js
+++ client/src/components/Pagination/Container.js
@@ -1,22 +1,35 @@
 import { getCardActionsUtilityClass } from "@mui/material";
 import { borderBottom } from "@mui/system";
 import React, {useState, useEffect} from "react"; 
-import Card from "../Card/Card"
-import {GET_ALL_CARDS} from "../../utils/queries"
+import Card from "../Card/Card";
+import {GET_ALL_CARDS} from "../../utils/queries";
 import { useQuery } from "@apollo/client";
 
 const CARDS_PER_PAGE = 1;
 const Container = () => {
     const {loading, data, error}= useQuery(GET_ALL_CARDS);
-    const [currPage, setCurrPage] = useState(1);
+    const [currPage, setCurrPage] = useState(0);
     const [totalCards, setTotalCards] = useState([]);
-    console.log(loading, data);
     let currCards = [];
     if(totalCards.length === 0  && !loading){ 
         setTotalCards(data.resourcesCards);
+        setCurrPage(1);
     }
-    const startingCardIndex = (currPage - 1) * CARDS_PER_PAGE;
-    currCards = data.resourcesCards.slice(startingCardIndex, startingCardIndex + CARDS_PER_PAGE);
+    if(currPage > 0) {
+
+        const startingCardIndex = (currPage - 1) * CARDS_PER_PAGE
+        currCards = data.resourcesCards.slice(startingCardIndex, startingCardIndex + CARDS_PER_PAGE );
+    }
+    
+    const handleClick = (inc) => {
+        const newStartIndex = (currPage + inc - 1) * CARDS_PER_PAGE;
+        const newEndIndex = newStartIndex + CARDS_PER_PAGE - 1;
+        if(newStartIndex < 0 || newEndIndex >= totalCards.length) {
+            //Ignore the click
+            return;
+        }
+        setCurrPage(currPage + inc);
+    }
 
     if(error) {
         console.log(error);
@@ -29,7 +42,10 @@ const Container = () => {
    } else {
        return (
            <div>
+
             {currCards.map(c => <Card key={c.id} title={c.title} description = {c.description} />)};
+            <button onClick={()=>handleClick(-1)} > Dec </button>
+            <button onClick={()=>handleClick(1)} >Inc</button> 
         </div>
         
         )
