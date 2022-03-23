e4bb1dc675b32ae1eddabea94f9d36ae23e1f84d
Olivier R.Measson <olivierberlin1998@hotmail.de>
2022-03-23 20:01:47 +0100

    changed autocomplete to checkbox component

--- client/src/components/Modals/Modal.js
+++ client/src/components/Modals/Modal.js
@@ -3,12 +3,19 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Modal from "@mui/material/Modal";
-import Autocomplete from "@mui/material/Autocomplete";
+// import Autocomplete from "@mui/material/Autocomplete";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
-import { Typography } from "@mui/material";
+// import { Typography } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
 
 const style = {
   position: "absolute",
@@ -42,10 +49,43 @@ const button = {
   transform: "translate(-50%, -50%)",
 };
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 export default function BasicModal() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [personLanguage, setPersonLanguage] = React.useState([]);
+  const [tags, setTag] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonLanguage(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  const handleChangement = (event1) => {
+    const {
+      target: { value },
+    } = event1;
+    setTag(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
 
   return (
     <div>
@@ -81,20 +121,61 @@ export default function BasicModal() {
             label="URL"
             variant="outlined"
           />
-          <Autocomplete
+
+          <FormControl sx={{ width: 400 }}>
+            <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+            <Select
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
+              multiple
+              value={tags}
+              onChange={handleChangement}
+              input={<OutlinedInput label="Tag" />}
+              renderValue={(selected) => selected.join(", ")}
+              MenuProps={MenuProps}
+            >
+              {allTags.map((tag) => (
+                <MenuItem key={tag} value={tag}>
+                  <Checkbox checked={tags.indexOf(tag) > -1} />
+                  <ListItemText primary={tag} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ width: 400 }}>
+            <InputLabel id="demo-multiple-checkbox-label">Language</InputLabel>
+            <Select
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
+              multiple
+              value={personLanguage}
+              onChange={handleChange}
+              input={<OutlinedInput label="Language" />}
+              renderValue={(selected) => selected.join(", ")}
+              MenuProps={MenuProps}
+            >
+              {languages.map((language) => (
+                <MenuItem key={language} value={language}>
+                  <Checkbox checked={personLanguage.indexOf(language) > -1} />
+                  <ListItemText primary={language} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={topLabel}
             sx={{ width: 400 }}
             renderInput={(params) => <TextField {...params} label="Tags" />}
-          />
-          <Autocomplete
+          /> */}
+          {/* <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={languages}
             sx={{ width: 400 }}
             renderInput={(params) => <TextField {...params} label="Language" />}
-          />
+          /> */}
           <Stack direction="row" spacing={2}>
             <Button variant="contained" endIcon={<SendIcon />}>
               Add
@@ -106,34 +187,21 @@ export default function BasicModal() {
   );
 }
 
-const topLabel = [
-  { label: "Ukraine" },
-  { label: "Children" },
-  { label: "Finance" },
-  { label: "Shelters" },
-  { label: "Germany" },
-  { label: "Animals" },
-  { label: "Refugees" },
-  {
-    label: "Medical",
-  },
-  { label: "Transport" },
-  { label: "Border" },
-  {
-    label: "Stay",
-  },
-  {
-    label: "Legal",
-  },
-  { label: "Place" },
-  { label: "Help" },
+const allTags = [
+  "Ukraine",
+  "Children",
+  "Finance",
+  "Shelters",
+  "Germany",
+  "Animals",
+  "Refugees",
+  "Medical",
+  "Transport",
+  "Border",
+  "Stay",
+  "Legal",
+  "Place",
+  "Help",
 ];
 
-const languages = [
-  { label: "English" },
-  { label: "German" },
-
-  { label: "Ukrainian" },
-  { label: "Polish" },
-  { label: "French" },
-];
+const languages = ["English", "French", "German", "Polish", "Ukrainian"];
