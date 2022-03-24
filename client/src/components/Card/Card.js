bcbe4d58de492c84040c77f53b9ab217fd0068ca
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2022-03-24 18:25:42 +0100

    First mui for card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,30 +1,68 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import FlagCircleRoundedIcon from '@mui/icons-material/FlagCircleRounded';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
+import Typography from "@mui/material/Typography";
+import FlagCircleRoundedIcon from "@mui/icons-material/FlagCircleRounded";
+import { positions } from "@mui/system";
 
-export default function MediaCard({title, description, screenshot, language}) {  
+const styleCard = {
+  maxWidth: 345,
+  borderRadius: "20px",
+  boxShadow: 2,
+  margin: 2,
+  backgroundColor: "transparent",
+  border: 1,
+  borderColor: "yellow",
+};
+
+const styleCardContent = {
+  backgroundColor: "white",
+  padding: "10px",
+  margin: "10px",
+  borderRadius: "20px",
+  opacity: "80%",
+};
+
+const styleMediaCard = {
+  margin: "10px",
+  marginTop: "50px",
+  border: 1,
+  borderRadius: "20px",
+};
+
+export default function MediaCard({
+  title,
+  description,
+  screenshot,
+  language,
+}) {
   return (
-    <Card sx={{ maxWidth: 345, borderRadius: 5, boxShadow: 2, margin: 2}}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={"screenshots/"+title.toLowerCase().replace(" ", "")+".png"}
-        alt="website image"
-      />
-      <CardContent>
+    <Card sx={styleCard}>
+      <div>
+        <CardMedia
+          sx={styleMediaCard}
+          component="img"
+          height="140"
+          image={"screenshots/" + title.toLowerCase().replace(" ", "") + ".png"}
+          alt="website image"
+        />
+      </div>
+      <CardContent sx={styleCardContent}>
         <Typography gutterBottom variant="h5" component="div">
-            {title}
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-            {description}
+          {description}
         </Typography>
       </CardContent>
-      <FlagCircleRoundedIcon/><FlagCircleRoundedIcon/><FlagCircleRoundedIcon/>
+      <Input defaultValue="Hello world" inputProps={} />
+      <FlagCircleRoundedIcon />
+      <FlagCircleRoundedIcon />
+      <FlagCircleRoundedIcon />
     </Card>
   );
 }
